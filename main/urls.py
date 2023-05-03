da9ffbab617ed18113f5ebed3ec6bb0d93ff2ef2
AnochiwaAlfred <42777193+AnochiwaAlfred@users.noreply.github.com>
2023-05-03 21:30:44 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add_todo', views.add_todo, name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
     path('completed_todo/<int:todo_id>/', views.completed_todo, name='completed_todo'),
+    path('incomplete_todo/<int:todo_id>/', views.incomplete_todo, name='incomplete_todo'),
 ]
